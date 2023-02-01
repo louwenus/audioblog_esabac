4d0821179a174dfb3dbb8a5f9f7df31ddd0c1224
louwenus <louwen.fricout@gmail.com>
2023-02-01 17:53:27 +0100

    1 test

--- main.php
+++ main.php
@@ -3,11 +3,13 @@
 $audios = json_decode(file_get_contents("./data.json"),true);
 
 foreach ($audios as $audio){
-echo "<div><h2>".$audio["nom"]."</h2>"
-echo "<audio controls>"
-foreach ($file as $audio["source"]
-echo "</audio>"
-echo "</div>"
+echo "<div><h2>".$audio["nom"]."</h2>";
+echo "<audio controls>";
+foreach ($audio["source"] as $file){
+    echo "<source ".$file."/>";
+}
+echo "</audio>";
+echo "</div>";
 }
 
 ?>
