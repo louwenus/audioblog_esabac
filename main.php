2cbdd44ba94e6d4eab9a9c51be8e2527e9196f57
louwenus <louwen.fricout@gmail.com>
2023-02-01 18:18:53 +0100

    more readable produced code

--- main.php
+++ main.php
@@ -2,12 +2,12 @@
 $audios = json_decode(file_get_contents("./data.json"),true);
 var_dump($audios);
 foreach ($audios as $audio){
-echo "<div><h2>".$audio["nom"]."</h2>";
-echo "<audio controls>";
+echo "<div><h2>".$audio["nom"]."</h2>\n";
+echo "  <audio controls>\n";
 foreach ($audio["source"] as $file){
-    echo "<source ".$file."/>";
+    echo "    <source ".$file."/>\n";
 }
-echo "</audio>";
+echo "  </audio>\n";
 echo "</div>";
 }
 
