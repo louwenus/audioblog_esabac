6292e206d9664a7f06baf70dbaf3a42426f10105
louwenus <louwen.fricout@gmail.com>
2023-02-02 21:45:18 +0100

    auto dark

--- jscript.js
+++ jscript.js
@@ -1,16 +1,20 @@
+//pause other players when one is started
 function onlyPlayOneIn(container) {
-    container.addEventListener("play", function(event) {
-    audio_elements = container.getElementsByTagName("audio")
-      for(i=0; i < audio_elements.length; i++) {
-        audio_element = audio_elements[i];
-        if (audio_element !== event.target) {
-          audio_element.pause();
-        }
+  container.addEventListener("play", function(event) {
+  audio_elements = container.getElementsByTagName("audio")
+    for(i=0; i < audio_elements.length; i++) {
+      audio_element = audio_elements[i];
+      if (audio_element !== event.target) {
+        audio_element.pause();
       }
-    }, true);
-  }
-  
-  document.addEventListener("DOMContentLoaded", function() {
-    onlyPlayOneIn(document.body);
-  });
-  
+    }
+  }, true);
+}
+document.addEventListener("DOMContentLoaded", function() {
+  onlyPlayOneIn(document.body);
+});
+
+//detect dark mode preference and auto toogle dark mode button
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  document.getElementById("toggle_darkmode").checked = true;
+}
