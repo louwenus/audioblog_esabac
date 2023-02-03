4f65eb9ca036ad7e80ada444c69c1dae9d072a64
louwenus <louwen.fricout@gmail.com>
2023-02-03 16:27:58 +0100

    simple json

--- main.php
+++ main.php
@@ -1,11 +1,10 @@
 <?php 
 $audios = json_decode(file_get_contents("./data.json"),true);
-echo $audios
 foreach ($audios as $audio){
 echo "<div class=\"audio_container\"><h2>".$audio["nom"]."</h2>\n";
 echo "  <audio controls>\n";
-    echo "    <source src=\"audio/".$audio["files"]."ogg\" type=\"audio/ogg\" >"
-    echo "    <source src=\"audio/".$audio["files"]."mp3\" type=\"audio/mp3\" >"
+    echo "    <source src=\"audio/".$audio["files"]."ogg\" type=\"audio/ogg\" >";
+    echo "    <source src=\"audio/".$audio["files"]."mp3\" type=\"audio/mp3\" >";
 echo "  </audio>\n";
 echo "</div>";
 }
