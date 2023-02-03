bf6fe5b8ae8e07a5455132e5f109e090bce72023
louwenus <louwen.fricout@gmail.com>
2023-02-03 16:25:40 +0100

    simple json

--- main.php
+++ main.php
@@ -1,5 +1,6 @@
 <?php 
 $audios = json_decode(file_get_contents("./data.json"),true);
+echo $audios
 foreach ($audios as $audio){
 echo "<div class=\"audio_container\"><h2>".$audio["nom"]."</h2>\n";
 echo "  <audio controls>\n";
