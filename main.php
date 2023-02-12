95bcce116bbdffb0a59e9a0fb56cba34d2c52824
louwenus <louwen.fricout@gmail.com>
2023-02-12 20:18:24 +0100

    styling tests

--- main.php
+++ main.php
@@ -7,13 +7,14 @@ echo "
         <label for=\"".$audio["files"]."\" class=\"img-label\">
             <img src=\"img/".$audio["files"].".jpg\">
         </label>
-        <label for=\"".$audio["files"]."\" class=\"title-label\">
-            <h2>".$audio["nom"]."</h2>
-        </label>
-        <p>".file_get_contents("./desc/".$audio["files"].".txt")."</p>
-        <audio controls>
-            <source src=\"audio/".$audio["files"].".ogg\" type=\"audio/ogg\" >
-            <source src=\"audio/".$audio["files"].".mp3\" type=\"audio/mp3\" >
-        </audio></div>";
+        <div>
+            <label for=\"".$audio["files"]."\" class=\"title-label\">
+                <h2>".$audio["nom"]."</h2>
+            </label>
+            <p>".file_get_contents("./desc/".$audio["files"].".txt")."</p>
+            <audio controls>
+                <source src=\"audio/".$audio["files"].".ogg\" type=\"audio/ogg\" >
+                <source src=\"audio/".$audio["files"].".mp3\" type=\"audio/mp3\" >
+            </audio></div></div>";
 }
 ?>
