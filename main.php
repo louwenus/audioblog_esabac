66717088e19578f882a31b04f8529463e14c65a4
louwenus <louwen.fricout@gmail.com>
2023-02-12 18:00:51 +0100

    gitignore

--- main.php
+++ main.php
@@ -1,8 +1,9 @@
 <?php 
 $audios = json_decode(file_get_contents("./data.json"),true);
 foreach ($audios as $audio){
-echo "<div class=\"audio_container\"><h2>".$audio["nom"]."</h2>\n";
-echo "  <audio controls>\n";
+echo "<div class=\"audio_container\"><h2>".$audio["nom"]."</h2>
+   this is a test
+   <audio controls>\n";
     echo "    <source src=\"audio/".$audio["files"].".ogg\" type=\"audio/ogg\" >\n";
     echo "    <source src=\"audio/".$audio["files"].".mp3\" type=\"audio/mp3\" >\n";
 echo "  </audio></div>\n";
