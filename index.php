474e1f1e6096197ef1179304f18b50800a795a4b
louwenus <louwen.fricout@gmail.com>
2023-03-04 11:56:12 +0100

    liscencing

--- index.php
+++ index.php
@@ -34,5 +34,8 @@
 
 	<?php include('./main.php'); ?>
 
+	<div id="bottom">
+		<p>Le code de la page est sous licsence GPLV3+, le contenu sous liscence  <a href="http://creativecommons.org/licenses/by-sa/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">CC BY-SA 4.0<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1"><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1"><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/sa.svg?ref=chooser-v1"></a></p>
+	</div>
 
 </body>
