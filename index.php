748b942370ebafcb0b2c66c69b0174f3a6263294
louwenus <louwen.fricout@gmail.com>
2023-03-04 10:22:28 +0100

    testing

--- index.php
+++ index.php
@@ -7,10 +7,9 @@
 
 
 <body>
+	<!--darkmode button-->
 	<input type="checkbox" id="toggle_darkmode">
 	<div id="background-color"></div>
-	<h1>Senza donne niente storie</h1>
-	
 	<label for="toggle_darkmode">
   		<div id="star">
     		<div class="star" id="star-1">★</div>
@@ -18,6 +17,19 @@
   		</div>
   		<div id="moon"></div>
 	</label>
+
+	<img id="banner" src="img/banner.png"/>
+	<h1>Pas d'histoire sans les femmes !</h1>
+	<h1>Senza donne niente storie !</h1>
+	<p>62 élèves de première, terminale, terza et quarta superiore des sections bi-nationales Esabac du Lycée du Granier
+		(La Ravoire/Savoie) et du liceo linguistico Della Rovere (Savona/Ligurie) ouvrent un audio-blogue bi-lingue
+		en italien et en français, sur le thème : Niente storia senza le donne/Pas d'histoire sans les femmes !
+		Cet audio-blogue se présente sous la forme d'une série de portraits de femmes françaises et italiennes dont
+		le rôle et la place dans l'Histoire ont tardé à être reconnus.
+		Les élèves, en groupe de 4 (2 correspondant.e.s français et leurs deux correspondant.e.s italiens) ont créé
+		chacun.e deux portraits, avec l'aide de la journaliste radiophonique italienne Monica Lanfranco (http://www.monicalanfranco.it/)
+		pour nous faire rencontrer 31 femmes aux destins peu ordinaires ... Un voyage au cours duquel se trame une prise de
+		conscience d'une Histoire commune à nos deux pays où les femmes, depuis bien longtemps, n'ont cessé d'être actrices !</p>
 
 
 	<?php include('./main.php'); ?>
