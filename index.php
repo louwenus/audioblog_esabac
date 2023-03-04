f129cddea077d482891c0db88dfac9d6ddfec077
louwenus <louwen.fricout@gmail.com>
2023-03-04 13:49:00 +0100

    margin content

--- index.php
+++ index.php
@@ -19,21 +19,22 @@
 	</label>
 
 	<img id="banner" src="img/banner.png"/>
-	<h1>Pas d'histoire sans les femmes !</h1>
-	<h1>Senza donne niente storie !</h1>
-	<p>62 élèves de première, terminale, terza et quarta superiore des sections bi-nationales Esabac du Lycée du Granier
-		(La Ravoire/Savoie) et du liceo linguistico Della Rovere (Savona/Ligurie) ouvrent un audio-blogue bi-lingue
-		en italien et en français, sur le thème : Niente storia senza le donne/Pas d'histoire sans les femmes !
-		Cet audio-blogue se présente sous la forme d'une série de portraits de femmes françaises et italiennes dont
-		le rôle et la place dans l'Histoire ont tardé à être reconnus.
-		Les élèves, en groupe de 4 (2 correspondant.e.s français et leurs deux correspondant.e.s italiens) ont créé
-		chacun.e deux portraits, avec l'aide de la journaliste radiophonique italienne Monica Lanfranco <a ref="http://www.monicalanfranco.it/">(http://www.monicalanfranco.it/)</a>
-		pour nous faire rencontrer 31 femmes aux destins peu ordinaires ... Un voyage au cours duquel se trame une prise de
-		conscience d'une Histoire commune à nos deux pays où les femmes, depuis bien longtemps, n'ont cessé d'être actrices !</p>
+	<div id="content">
+		<h1>Pas d'histoire sans les femmes !</h1>
+		<h1>Senza donne niente storie !</h1>
+		<p>62 élèves de première, terminale, terza et quarta superiore des sections bi-nationales Esabac du Lycée du Granier
+			(La Ravoire/Savoie) et du liceo linguistico Della Rovere (Savona/Ligurie) ouvrent un audio-blogue bi-lingue
+			en italien et en français, sur le thème : Niente storia senza le donne/Pas d'histoire sans les femmes !
+			Cet audio-blogue se présente sous la forme d'une série de portraits de femmes françaises et italiennes dont
+			le rôle et la place dans l'Histoire ont tardé à être reconnus.
+			Les élèves, en groupe de 4 (2 correspondant.e.s français et leurs deux correspondant.e.s italiens) ont créé
+			chacun.e deux portraits, avec l'aide de la journaliste radiophonique italienne Monica Lanfranco <a ref="http://www.monicalanfranco.it/">(http://www.monicalanfranco.it/)</a>
+			pour nous faire rencontrer 31 femmes aux destins peu ordinaires ... Un voyage au cours duquel se trame une prise de
+			conscience d'une Histoire commune à nos deux pays où les femmes, depuis bien longtemps, n'ont cessé d'être actrices !</p>
 
 
-	<?php include('./main.php'); ?>
-
+			<?php include('./main.php'); ?>
+	</div>
 	<div id="bottom">
 		<p>Le code de la page est sous licence <a rel="license" href="https://www.gnu.org/licenses/gpl-3.0.html">GPLV3+ ,</a><br>
 		Le contenu de la page est mis à disposition selon les termes de la <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
