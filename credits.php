21d530734edb4590ef4a05ca8a027a87908c9567
louwenus <louwen.fricout@gmail.com>
2023-04-28 20:34:46 +0200

    reworked style + credits page

---
+++ credits.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<head>
+	<title>Credits - Senza donne niente storia</title>
+	<link rel="stylesheet" href="style.css">
+	<script src="jscript.js"></script> 
+</head>
+
+
+<body>
+	<!--darkmode button-->
+	<input type="checkbox" id="toggle_darkmode">
+	<div id="background-color"></div>
+	<label for="toggle_darkmode">
+  		<div id="star">
+    		<div class="star" id="star-1">★</div>
+    		<div class="star" id="star-2">★</div>
+  		</div>
+  		<div id="moon"></div>
+	</label>
+
+	<img id="banner" src="img/banner.png"/>
+	<div id="content">
+		<h1>Pas d'histoire sans les femmes !</h1>
+		<h1>Senza donne niente storia !</h1>
+		<p>Voici les crédits par podcasts. <a href="./">Retour aux podcasts</a></p>
+
+        <?php 
+            $audios = json_decode(file_get_contents("./data.json"),true);
+            foreach ($audios as $audio){
+            echo '<h2 class=>'.$audio['nom'].'</h2>
+                 '.file_get_contents('./credits/'.$audio['files'].'.html');
+            }
+        ?>
+	</div>
+	<div id="bottom">
+		<p><a href="./">Retour aux podcasts</a><br/>
+		Le code de la page est sous licence <a rel="license" href="https://www.gnu.org/licenses/gpl-3.0.html">GPLV3+ ,</a><br>
+		Le contenu de la page est mis à disposition selon les termes de la <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
+		Licence Creative Commons CC-BY-SA <img style="margin-left:10px;" alt="" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a></p>
+	</div>
+</body>
--- main.php
+++ main.php
@@ -1,24 +1,25 @@
 <?php 
 $audios = json_decode(file_get_contents("./data.json"),true);
 foreach ($audios as $audio){
-echo "
-    <hr />
-    <input type=\"radio\" id=\"".$audio["files"]."\" value=\"".$audio["files"]."\" name=\"radio-audio-group\">
-    <div class=\"audio_container\">
-        <label for=\"".$audio["files"]."\" class=\"img-label\">
-            <img src=\"img/".$audio["files"].".jpg\">
-        </label>
-        <div>
-            <label for=\"".$audio["files"]."\" class=\"title-label\">
-                <h2>".$audio["nom"]."</h2>
-            </label>
-            <div class=\"to_hide\">
-                ".file_get_contents("./desc/".$audio["files"].".html")."
+echo '
+    <div class="audio_container">
+        <input type="radio" id="'.$audio['files'].'" value="'.$audio['files'].'" name="radio-audio-group">
+        <label for="'.$audio['files'].'">
+
+            <img src="img/'.$audio['files'].'.jpg">
+            
+            <div class="to_hide">
+                <h2 class="title">'.$audio['nom'].'</h2>
+                '.file_get_contents('./desc/'.$audio['files'].'.html').'
+                <p>Par: '.$audio['auteurs'].'</p>
+
+                <audio controls>
+                    <source src="audio/'.$audio['files'].'.ogg" type="audio/ogg" >
+                    <source src="audio/'.$audio['files'].'.mp3" type="audio/mp3" >
+                </audio>
             </div>
-            <p>Auteurs: ".$audio["auteurs"]."</p>
-            <audio controls class=\"to_hide\">
-                <source src=\"audio/".$audio["files"].".ogg\" type=\"audio/ogg\" >
-                <source src=\"audio/".$audio["files"].".mp3\" type=\"audio/mp3\" >
-            </audio></div></div>";
+
+        </label>
+    </div>';
 }
 ?>
